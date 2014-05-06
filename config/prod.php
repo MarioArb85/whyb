9fed7cda75e1fb7213e98e64207961036543f92d
camu85 <marioarbesu85@gmail.com>
2014-05-06 19:02:54 +0200

    2...

---
+++ config/prod.php
@@ -0,0 +1,9 @@
+<?php
+
+// configure your app for the production environment
+
+$app['twig.path'] = array(__DIR__.'/../templates');
+$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+//Variables mias
+define("DIR_RAIZ_APP", '/whyb/');
