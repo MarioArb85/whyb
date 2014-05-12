c9ad14c06a9dfd375bed903c10dbc6e2d9a56375
camu85 <marioarbesu85@gmail.com>
2014-05-13 00:20:05 +0200

    9

--- src/vistas/places/places.php
+++ src/vistas/places/places.php
@@ -9,37 +9,36 @@ class places {
     $xml = simplexml_load_file("c:/xampp/htdocs/whyb/src/unesco/unesco_places_ES.xml");
     $xmle = simplexml_load_file("c:/xampp/htdocs/whyb/src/unesco/unesco_places_EN.xml");
 
-    //$consulta = "INSERT INTO places (`typeId`, `name`, `region`, `country`, `latitude`, `longitude`, `unescoimage`, `web`) VALUES ";
-    //$consulta = "UPDATE countries SET `countryName_es`='' WHERE `countryId`='AD';";
-    $cont=2;
-    $guardado ="Afganistán";
-    for ($i=0; $i<=921; $i++){
-        //Guardando los datos en variables
-        //$categoria = $xml->row[$i]->category;
-        //$sitio = $xml->row[$i]->site;
-        //$latitud = $xml->row[$i]->latitude;
-        //$longitud = $xml->row[$i]->longitude;
-        $paisIso = $xml->row[$i]->iso_code;
-        //$web = $xml->row[$i]->http_url;
-        //$imagen = $xml->row[$i]->image_url;
-        //$continente = $xml->row[$i]->region;
-        $paisNombre = $xml->row[$i]->states;
-        //$paisIsoEN = $xmle->row[$i]->iso_code;
-        //$paisNombreEN = $xmle->row[$i]->states;
-        //$sitioEN = $xmle->row[$i]->site;
-        //$imagenEN = $xmle->row[$i]->image_url;
-    	
-        $firephp->log($guardado, 'guardado');
-        $firephp->log($paisNombre, 'pais');    
+    //conexion bbdd
+    $conexion = new mysqli('localhost', 'root', '', 'whyb');
+    $conexion->set_charset('utf8');
 
-        if ($guardado == $paisNombre) {
-            $guardado = $paisNombre;
-            $firephp->log($cont, 'contador');
-            $cont++;
-        }
+    $consulta2 = "INSERT INTO places (`typeId`, `countryId`, `continentId`, `latitude`, `longitude`, `unesco_es`, `unescoimage`, `web_es`, `web_en`) VALUES ";
+
+    if ($conexion->connect_error){
+        die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+    }
+
+    $cont=0;
+    for ($i=0; $i<=921; $i++){
+        $cont++;
+        //Guardando los datos en variables
+        $categoria = $xml->row[$i]->category;
+        $sitio = $xml->row[$i]->site;
+        $latitud = $xml->row[$i]->latitude;
+        $longitud = $xml->row[$i]->longitude;
+        $paisIso = $xml->row[$i]->iso_code;
+        $webEN = $xml->row[$i]->http_url;
+        $imagen = $xml->row[$i]->image_url;
+
+        $paisIsoEN = $xmle->row[$i]->iso_code;
+        //$sitioEN = $xmle->row[$i]->site;
+        //$webEN = $xmle->row[$i]->http_url;
+
+        $sitioEN = "";
 
         //Codificar categoría
-/*        switch ($categoria) {
+        switch ($categoria) {
             case 'Natural':
                 $tipo = 1; 
                 break;
@@ -54,65 +53,58 @@ class places {
                 break;
         }
 
-        //Traducir continente
-        switch ($continente) {
-            case 'Europe and North America':
-                $region = "Europa y América del Norte"; 
-                break;
-            case 'Asia and the Pacific':
-                $region = "Asia-Pacífico";
-                break;
-            case 'Africa':
-                $region = "África"; 
-                break;
-            case 'Arab States':
-                $region = "Estados Árabes";
-                break;
-            case 'Latin America and the Caribbean':
-                $region = "Estados Árabes"; 
-                break;
-            default:
-                $region = "none"; 
-                break;
+        /*$cont=0;
+        for ($i=0; $i<=921; $i++){
+            $cont++;
+            $firephp->log($web, 'consulta');
+            $firephp->log($xmle->row[$i]->http_url, 'consulta');
+
+            if((string)$web == (string)$xmle->row[$i]->http_url){
+                $firephp->log('Entrooooooooooooooooooooooooooooooooooooo', 'consulta');
+                $firephp->log($cont, 'consulta');
+                $sitioEN = $xmle->row[$i]->site;
+                break; 
+            }
         }
-*/
+        */
+        //Convertir pagina a español
+        $web = str_replace("/en/","/es/",$webEN);
+
+        //Covertir codigo pais a mayusculas
+        $paisIso = strtoupper($paisIso);
+
+        //Recuperar el Id del continente al que pertenece el pais
+        $consulta1 = "SELECT continentId FROM countries WHERE countryId='$paisIso'";
+        $result = $conexion->query($consulta1);
+        $pasar = mysqli_fetch_array($result);
+        $continent = $pasar['continentId'];
+
         //Generar consulta para introducir los datos
-    	//$consulta .= " ($tipo, '$sitio', '$region', '$paisIso', $latitud, $longitud, '$imagen', '$web'),";
+    	$consulta2 .= " ($tipo, '$paisIso', $continent, $latitud, $longitud, '$sitio', '$imagen', '$web', '$webEN'),";
 
-/*
         //Imprimir los resultados ordenados por lugar
-    	//$body .= "Categoria: $tipo<br/>";
-    	$body .= "Sitio: $sitio<br/>";
-		//$body .= "Latitud: $latitud<br/>";
-		//$body .= "Longitud: $longitud<br/>";
-		$body .= "Pais ISO: $paisIso<br/>";
-        $body .= "Pais nombre: $paisNombre<br/>";
-		$body .= "Web: $web<br/>";
-		//$body .= "Imagen: <a href='$imagen'>Imagen del lugar</a><br/>";
-		$body .= "Region: $region<br/>";
+    	$body .= "Categoria ID: $tipo<br/>";
+        $body .= "Pais ISO: $paisIso<br/>";
+        $body .= "Continente ID: ".$pasar['continentId']."<br/>";
+		$body .= "Latitud: $latitud<br/>";
+		$body .= "Longitud: $longitud<br/>";
+		$body .= "Sitio: $sitio<br/>";
         $body .= "Sitio (en): $sitioEN<br/>";
-        $body .= "Pais ISO (en): $paisIsoEN<br/>";
-        $body .= "Pais nombre (en): $paisNombreEN<br/>";
+		$body .= "Imagen: $imagen<br/>";
+        $body .= "Web: $web<br/>";
+        $body .= "Web (en): $webEN<br/>";
 		$body .= "<hr/>";
-*/
 	}
-/*
+
     //Cambiar la ultima , por ;
-	$consulta = substr($consulta, 0, -1);
-	$consulta .= ";";
+	$consulta2 = substr($consulta2, 0, -1);
+	$consulta2 .= ";";
 
-	//$firephp->log($consulta, 'consulta');
+	$firephp->log($cont, 'consulta');
 
-	//conexion bbdd
-	$conexion = new mysqli('localhost', 'root', '', 'whyb');
-	$conexion->set_charset('utf8');
+	$conexion->query($consulta2);
+    
 
-	if ($conexion->connect_error){
-		die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-	}
-	
-	$conexion->query($consulta);
-*/
 	//return $consulta;
     return $body;
   }
