524fcde552d21d6f05cc072d992e09d8a673564b
camu85 <marioarbesu85@gmail.com>
2014-05-13 00:59:41 +0200

    10

--- src/vistas/places/places.php
+++ src/vistas/places/places.php
@@ -13,7 +13,7 @@ class places {
     $conexion = new mysqli('localhost', 'root', '', 'whyb');
     $conexion->set_charset('utf8');
 
-    $consulta2 = "INSERT INTO places (`typeId`, `countryId`, `continentId`, `latitude`, `longitude`, `unesco_es`, `unescoimage`, `web_es`, `web_en`) VALUES ";
+    //$consulta2 = "INSERT INTO places (`typeId`, `countryId`, `continentId`, `latitude`, `longitude`, `unesco_es`, `unescoimage`, `web_es`, `web_en`) VALUES ";
 
     if ($conexion->connect_error){
         die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
@@ -22,21 +22,25 @@ class places {
     $cont=0;
     for ($i=0; $i<=921; $i++){
         $cont++;
-        //Guardando los datos en variables
-        $categoria = $xml->row[$i]->category;
-        $sitio = $xml->row[$i]->site;
-        $latitud = $xml->row[$i]->latitude;
-        $longitud = $xml->row[$i]->longitude;
-        $paisIso = $xml->row[$i]->iso_code;
-        $webEN = $xml->row[$i]->http_url;
-        $imagen = $xml->row[$i]->image_url;
+        //Guardando los datos en variables ($consulta2)
+        //$categoria = $xml->row[$i]->category;
+        //$sitio = $xml->row[$i]->site;
+        //$latitud = $xml->row[$i]->latitude;
+        //$longitud = $xml->row[$i]->longitude;
+        //$paisIso = $xml->row[$i]->iso_code;
+        //$webEN = $xml->row[$i]->http_url;
+        //$imagen = $xml->row[$i]->image_url;
+        
 
-        $paisIsoEN = $xmle->row[$i]->iso_code;
-        //$sitioEN = $xmle->row[$i]->site;
-        //$webEN = $xmle->row[$i]->http_url;
+        //Guardando los datos en variables ($consulta3)
+        $sitioEN = $xmle->row[$i]->site;
+        $webEN = $xmle->row[$i]->http_url;
 
-        $sitioEN = "";
+        //quitar comillas
+        $sitioEN = str_replace("'"," ",$sitioEN);
+        $sitioEN = str_replace("’"," ",$sitioEN);
 
+/*
         //Codificar categoría
         switch ($categoria) {
             case 'Natural':
@@ -52,21 +56,8 @@ class places {
                 $tipo = 0;
                 break;
         }
-
-        /*$cont=0;
-        for ($i=0; $i<=921; $i++){
-            $cont++;
-            $firephp->log($web, 'consulta');
-            $firephp->log($xmle->row[$i]->http_url, 'consulta');
-
-            if((string)$web == (string)$xmle->row[$i]->http_url){
-                $firephp->log('Entrooooooooooooooooooooooooooooooooooooo', 'consulta');
-                $firephp->log($cont, 'consulta');
-                $sitioEN = $xmle->row[$i]->site;
-                break; 
-            }
-        }
-        */
+*/
+        /*
         //Convertir pagina a español
         $web = str_replace("/en/","/es/",$webEN);
 
@@ -81,21 +72,28 @@ class places {
 
         //Generar consulta para introducir los datos
     	$consulta2 .= " ($tipo, '$paisIso', $continent, $latitud, $longitud, '$sitio', '$imagen', '$web', '$webEN'),";
+*/
+
+        //Generar consulta para introducir los datos
+        $consulta3 = "UPDATE places SET unesco_en='$sitioEN' WHERE  web_en='$webEN'";
+        $firephp->log($consulta3, 'consulta');
+        $conexion->query($consulta3);
 
         //Imprimir los resultados ordenados por lugar
-    	$body .= "Categoria ID: $tipo<br/>";
-        $body .= "Pais ISO: $paisIso<br/>";
-        $body .= "Continente ID: ".$pasar['continentId']."<br/>";
-		$body .= "Latitud: $latitud<br/>";
-		$body .= "Longitud: $longitud<br/>";
-		$body .= "Sitio: $sitio<br/>";
+    	//$body .= "Categoria ID: $tipo<br/>";
+        //$body .= "Pais ISO: $paisIso<br/>";
+        //$body .= "Continente ID: ".$pasar['continentId']."<br/>";
+		//$body .= "Latitud: $latitud<br/>";
+		//$body .= "Longitud: $longitud<br/>";
+		//$body .= "Sitio: $sitio<br/>";
         $body .= "Sitio (en): $sitioEN<br/>";
-		$body .= "Imagen: $imagen<br/>";
-        $body .= "Web: $web<br/>";
+		//$body .= "Imagen: $imagen<br/>";
+        //$body .= "Web: $web<br/>";
         $body .= "Web (en): $webEN<br/>";
 		$body .= "<hr/>";
-	}
 
+	}
+/*
     //Cambiar la ultima , por ;
 	$consulta2 = substr($consulta2, 0, -1);
 	$consulta2 .= ";";
@@ -103,7 +101,7 @@ class places {
 	$firephp->log($cont, 'consulta');
 
 	$conexion->query($consulta2);
-    
+*/    
 
 	//return $consulta;
     return $body;
