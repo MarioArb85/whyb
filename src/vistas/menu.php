9f1b81232be50cd695a6f14f8802399c8aaaec2f
camu85 <marioarbesu85@gmail.com>
2014-05-29 04:03:03 +0200

    42

--- src/vistas/menu.php
+++ src/vistas/menu.php
@@ -17,7 +17,7 @@ class menu {
           <a href='".DIR_PLACES."' class='link $places'>Mis lugares</a>
         </div>
         <div id='linkRoutes'>
-          <a href='javascript: void(0)' class='link'  id='linkRoutesa $route'>Mis rutas</a>
+          <a href='javascript: void(0)' class='link'  id='linkRoutesa' $route'>Mis rutas</a>
         </div>
       </div>  
     </div>";
